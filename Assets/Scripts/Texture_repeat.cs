b9efcb618ee406c6785bc8068bf3e2c0ea17a3b8
Benoit Boyer <perso@benoitbm.fr>
2016-03-02 02:51:58 +0000

    First level - first try of creation
    A (very bad) example for the first level, to give an idea about how it
    can looks.
    Still have elements to add (like cars, and bad guys, and outside walls)
    and to optimize.
    We can randomly generate the graves and place them randomly.

---
+++ Assets/Scripts/Texture_repeat.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//This script is used when you want to have a mosaic texture on any surface.
+
+public class Texture_repeat : MonoBehaviour {
+    /// <summary>
+    /// XRepeat is the number you want to repeat on x-axis.
+    /// </summary>
+    public float XRepeat = 64;
+    /// <summary>
+    /// YRepeat is the number you want to repeat on y-axis.
+    /// </summary>
+    public float YRepeat = 64;
+
+	// Use this for initialization
+	void Start () {
+        transform.GetComponent<Renderer>().material.mainTextureScale = new Vector2(XRepeat, YRepeat);
+    }
+
+}
