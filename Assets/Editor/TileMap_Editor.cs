f9d96dd5e63d883e16c2edb4f10cc8daa3a8bae0
Benoit Boyer <perso@benoitbm.fr>
2016-03-05 19:55:51 +0000

    First part tile map editor
    A beginning, you can create a new Tilemap, and load a tilemap as a
    texture, and Unity will detecte the site of the tile map, a beginning
    for this.

---
+++ Assets/Editor/TileMap_Editor.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+//Script for editing Tilemap in Unity. Won't affect the game.
+
+[CustomEditor(typeof(TileMap))]
+public class TileMap_Editor : Editor {
+
+    public TileMap tmap; //For the tilemap created before
+
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.BeginVertical();
+        tmap.mapSize = EditorGUILayout.Vector2Field("Map Size:", tmap.mapSize);
+        tmap.texture2D = (Texture2D)EditorGUILayout.ObjectField("Tile map texture:", tmap.texture2D, typeof(Texture2D), false);
+
+        if (tmap.texture2D == null) //Checking that a texture is chosen, if not...
+        {
+            //It will show a message inside the editor
+            EditorGUILayout.HelpBox("A Texture 2D is not selected. Please select one.", MessageType.Warning);   
+        }
+        else
+        {
+            EditorGUILayout.LabelField("Tile Size :", tmap.tileSize.x+"x"+tmap.tileSize.y);
+        }
+
+        EditorGUILayout.EndVertical();
+    }
+
+    //Called when is displayed in Unity
+    void OnEnable()
+    {
+        tmap = target as TileMap;
+        Tools.current = Tool.View; //To be sure that we move elements (and not resize them, rotate or another)
+
+        if (tmap.texture2D != null)
+        {
+            var path = AssetDatabase.GetAssetPath(tmap.texture2D);
+            tmap.spriteReferences = AssetDatabase.LoadAllAssetsAtPath(path);
+
+            var sprite = (Sprite)tmap.spriteReferences[1]; //We take the 2nd value because the 1st one is a reference
+            var w = sprite.textureRect.width;
+            var h = sprite.textureRect.height;
+
+            tmap.tileSize = new Vector2(w, h); //Updating the tile size in the file
+        }
+
+    }
+}
---
+++ Assets/Scripts/TileMap.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileMap : MonoBehaviour {
+
+    public Vector2 mapSize = new Vector2(200, 100); //Number of tiles for the map 
+    public Texture2D texture2D;
+    public Vector2 tileSize = new Vector2();
+    public Object[] spriteReferences; //Will contain informations about placed elements
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
