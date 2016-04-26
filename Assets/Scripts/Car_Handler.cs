7688f08e96df09ad6dde3e0db4fa858f4f6b60a0
Benoit Boyer <perso@benoitbm.fr>
2016-04-26 13:08:17 +0100

    Car transition commit
    Near the end, gotta need a small help from a car to go further.
    
    Features of this commit :
    - Car sprites added
    - Transition between levels added
    - Code cleaning
    - Removing of empty scripts

---
+++ Assets/Scripts/Car_Handler.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+//This script is used to switch states 
+
+public class Car_Handler : MonoBehaviour {
+
+    public GameObject carOpened;
+    public GameObject carClosed;
+    public GameObject transitionScreen;
+    bool leaving = false;
+
+    void LateUpdate()
+    {
+        if (leaving)
+        {
+            var pos = gameObject.transform.position;
+            pos.x += Time.deltaTime * 2;
+            gameObject.transform.position = pos;
+        }
+    }
+
+    public void openCar()
+    {
+        Destroy(gameObject.transform.GetChild(0).gameObject);
+        var temp = Instantiate(carOpened);
+        temp.transform.SetParent(gameObject.transform);
+        temp.transform.localPosition = Vector3.zero;
+    }
+
+    public void leave()
+    {
+        Destroy(gameObject.transform.GetChild(0).gameObject);
+        var temp = Instantiate(carClosed);
+        temp.transform.SetParent(gameObject.transform);
+        temp.transform.localPosition = Vector3.zero;
+        leaving = true;
+
+        GameObject.FindObjectOfType<Goal_Script>().levelComplete();
+        GameObject.FindObjectOfType<FollowPlayer>().stopFollow();
+
+        transitionScreen.SetActive(true);
+        transitionScreen.GetComponent<ScreenFader>().showScreen();
+
+        StartCoroutine(gotoNextLevel());
+    }
+
+    IEnumerator gotoNextLevel()
+    {
+        yield return new WaitForSeconds(2.5f);
+
+        var id = SceneManager.GetActiveScene().buildIndex;
+        if (id >= (SceneManager.sceneCountInBuildSettings-1))
+            SceneManager.LoadScene("MainMenu"); //TODO Load credits screen
+        else
+            SceneManager.LoadScene(id + 1);
+    }
+
+}
